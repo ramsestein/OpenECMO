d80d4035398e1c0993c7e60f2572a5598176a6fc
Iñaki Gonzalez <limako50@gmail.com>
2020-05-06 09:50:59 +0000

    Upload New File

---
+++ Perfusor/Arduino files/src/Pinout.h
@@ -0,0 +1,22 @@
+/**
+ * @file Pinout.h
+ * @author Limako
+ * @author Sergio Gasquez Arcos (sergio.gasquez@gmail.es)
+ * @brief Connections for the system
+ * @version 0.1
+ * @date 2020-05-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef PINOUT_H
+#define PINOUT_H
+
+#define STEPPER_FORWARD_BUTTON_PIN 6
+#define STEPPER_BACKWARD_BUTTON_PIN 7
+#define ACCEPT_BUTTON_PIN 8
+#define POTENTIOMETER_PIN A0
+
+
+#endif   // PINOUT_H
