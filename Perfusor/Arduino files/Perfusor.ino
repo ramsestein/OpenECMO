38e969e4207333d2acbee88e9d9c66c153800b90
Iñaki Gonzalez <limako50@gmail.com>
2020-05-13 11:51:48 +0000

    Replace Perfusor.ino

--- Perfusor/Arduino files/Perfusor.ino
+++ Perfusor/Arduino files/Perfusor.ino
@@ -23,6 +23,7 @@ SyringeDriver syringeDriver(LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS, AccelStepper::D
 
 void setup()
 {
+  attachInterrupt(digitalPinToInterrupt(3), encoder, CHANGE);
   syringeDriver.initialize();
   syringeDriver.homing();
 }
@@ -43,63 +44,25 @@ void loop()
     syringeDriver.updateFlow();
     syringeDriver.updateParameters();
   }
-  else if (state == static_cast<uint8_t>(State::ADJUSTING) && digitalRead(ACCEPT_BUTTON_PIN))
+  else if (state == static_cast<uint8_t>(State::ADJUSTING))
   {
-    syringeDriver.setMaxSpeed(3 * syringeDriver.getStepsPerMl());   // Speed: 3 mm/s
-    syringeDriver.setAcceleration(3 * syringeDriver.getStepsPerMl());
-    float startingPosition = syringeDriver.getStartingPosition();
-    syringeDriver.moveTo(startingPosition);
-    while (syringeDriver.currentPosition() != startingPosition && !digitalRead(STEPPER_BACKWARD_BUTTON_PIN))
-    {
-      syringeDriver.run();
-    }
-    syringeDriver.stop();
-    syringeDriver.setState(State::FINE_ADJUSTING);
+    syringeDriver.Aprox();
   }
   else if (state == static_cast<uint8_t>(State::FINE_ADJUSTING))
   {
-    syringeDriver.setMaxSpeed(1.5 * syringeDriver.getStepsPerMl());   // Speed: 3 mm/s
-    syringeDriver.setAcceleration(1.5 * syringeDriver.getStepsPerMl());
-    if (digitalRead(STEPPER_FORWARD_BUTTON_PIN))
-    {
-      syringeDriver.move(10);
-      syringeDriver.run();
-    }
-    else if (digitalRead(STEPPER_BACKWARD_BUTTON_PIN))
-    {
-      syringeDriver.move(-10);
-      syringeDriver.run();
-    }
-    else
-    {
-      syringeDriver.stop();
-    }
-    if (digitalRead(ACCEPT_BUTTON_PIN))
-    {
-      while (digitalRead(ACCEPT_BUTTON_PIN))
-      {
-        delay(1);
-      }
-      syringeDriver.setState(State::DRAIN_SYRINGE);
-    }
+    
+    syringeDriver.FineAdj();
   }
   else if (state == static_cast<int>(State::DRAIN_SYRINGE) && digitalRead(ACCEPT_BUTTON_PIN))
   {
-    /// @todo: When some button is pressed return to flow selection rate
-    /// @todo: When some button is pressed go into a manual mode where pressing buttons you can drain the syringe at higher speed
-    /// @todo: Check progress constantly to be able to alarm when a threshold is reached
-    syringeDriver.setCurrentPosition(syringeDriver.getLength());
-    syringeDriver.setMaxSpeed(syringeDriver.getMaxSpeed());   // Speed obtained with the flow rate
-    syringeDriver.setAcceleration(syringeDriver.getMaxSpeed());
-    syringeDriver.moveTo(0);
-    while (syringeDriver.currentPosition() != 0 && !digitalRead(STEPPER_BACKWARD_BUTTON_PIN))   // Aqui seria un final de carrera
-    {
-      syringeDriver.run();
-      syringeDriver.displayDrainingInfo();
-    }
-    syringeDriver.stop();
+    syringeDriver.Drain();
   }
   else
   {
   }
+}
+
+void encoder()
+{
+  syringeDriver.encoder();
 }
