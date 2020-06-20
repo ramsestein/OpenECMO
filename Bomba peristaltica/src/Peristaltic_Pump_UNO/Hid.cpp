0afff2802eef8f8b9564624cd2ddd241ee13f1dc
Francisco Ochando <francisco.ochando@gmail.com>
2020-06-20 07:03:31 +0000

    Update Hid.cpp (mejora de selección de RPMs con potenciómetro y pulsadores)

--- Bomba peristaltica/src/Peristaltic_Pump_UNO/Hid.cpp
+++ Bomba peristaltica/src/Peristaltic_Pump_UNO/Hid.cpp
@@ -1,20 +1,18 @@
 /*
     Author: Francisco Ochando
     
-    Hid.cpp
-    Contine los métodos de LCD. encoder y botonera
+    Module: Hid.cpp
+    Description: Contine los métodos de LCD. encoder y botonera
 
  */
+
 #include "Peristaltic_pump.h"
 #include "Pump.h"
-
-//#define SW_REV_PULSADO 1
-//#define SW_ENC_PULSADO 2
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 // LiquidCrystal_I2C lcd(0x27,16,2);
 // Set the LCD address to 0x27 for a 20 chars and 4 line display
-//LiquidCrystal_I2C lcd(0x27,20,4);
+// LiquidCrystal_I2C lcd(0x27,20,4);
 Pump pump_;
 
 void Hid::Setup() {
@@ -33,28 +31,28 @@ void Hid::Setup() {
 void Hid::Init()
 {
   
-  #if LCD_20x4  
+#if LCD_20x4  
   LCD.setCursor( 0 , 2 );
   LCD.print(F("RPM: "));
   LCD.setCursor( 0 , 3 );
   LCD.print(F("VOL: "));
   LCD.setCursor( 10 , 3 );
   LCD.print( "mL/min");
-  #else   // En caso de LCD 16x2
+#else   // En caso de LCD 16x2
   LCD.setCursor( 0 , 0 );
   LCD.print(F("RPM: "));
   LCD.setCursor( 0 , 1 );
   LCD.print(F("Q: "));
   LCD.setCursor( 10 , 1 );
   LCD.print( "mL/min");  
-  #endif
+#endif
 
 }
 
 void Hid::Update( byte value1, byte value2 )
 {
   
-  #if LCD_20x4  
+#if LCD_20x4  
   LCD.setCursor( 0,2 );
   LCD.print(F("RPM: "));
   LCD.print( value1 );
@@ -62,7 +60,7 @@ void Hid::Update( byte value1, byte value2 )
   LCD.print(F("VOL: "));
   LCD.print( value2 );
   LCD.print( " mL/min");
-  #else   // En caso de LCD 16x2
+#else   // En caso de LCD 16x2
   LCD.setCursor( 0,0 );
   LCD.print(F("RPM: "));
   LCD.print( value1 );
@@ -70,50 +68,51 @@ void Hid::Update( byte value1, byte value2 )
   LCD.print(F("Q: "));
   LCD.print( value2 );
   LCD.print( " mL/min");  
-  #endif
+#endif
 
 }
 
 void Hid::PrintRPM( int value1 )
 {
   
-  #if LCD_20x4  
+#if LCD_20x4  
   LCD.setCursor( 5 , 2 );
   LCD.print( value1 );
   LCD.print( "  " );
-  #else   // En caso de LCD 16x2
+#else   // En caso de LCD 16x2
   LCD.setCursor( 5 , 0 );
   LCD.print( value1 );
   LCD.print( "  " );  
-  #endif
+#endif
 
 }
 
 void Hid::PrintVol( float value1 )
 {
 
-  #if LCD_20x4 
+#if LCD_20x4 
   LCD.setCursor( 5 , 3 );
+  //LCD.print( "               ");
   LCD.print( value1 );
-  LCD.print( " mL/min");
-  #else   // En caso de LCD 16x2
+  LCD.print( " mL/min" );
+#else   // En caso de LCD 16x2
   LCD.setCursor( 3 , 1 );
   LCD.print( value1 );
-  LCD.print( " mL/min");  
-  #endif
+  LCD.print( " mL/min" );  
+#endif
 
 }
 
-void Hid::PrintAlarm( void )
+void Hid::setAlarm( void )
 {
 
-  #if LCD_20x4 
+#if LCD_20x4 
   LCD.setCursor( 11 , 2 );
   LCD.print( "< ALARM >");
-  #else   // En caso de LCD 16x2
+#else   // En caso de LCD 16x2
   LCD.setCursor( 9 , 0 );
   LCD.print( "<ALARM>");  
-  #endif
+#endif
 
 }
 
@@ -121,30 +120,40 @@ void Hid::setDir( bool dir )
 {
   
   LCD.setCursor( 17 , 0 );
-  if(dir) {
-    LCD.print( ">>>");   
-  } else {
-    LCD.print( "<<<");    
+  if(dir) {   // dir = true
+    LCD.print( ">>>" );
+  } else {    // dir = false
+    LCD.print( "<<<" );   
   }
 
 }
 
-byte Hid::Lee( void )
+void Hid::setON( bool on )
+{
+  
+  LCD.setCursor( 18 , 1 );
+  if(on) {   // dir = true
+    LCD.print( "ON" );
+  } else {
+    LCD.print( "  " );
+  }
+
+}
+
+void Hid::Read( void )
 {
   
   //    Switch Inversion giro
   if ( !digitalRead(SW_REV) ) {
     but = SW_REV_PULSADO;
-    pulsado++;
-  }
-             
+    pressed++;
+  }           
   //    Switch Encoder
   if ( !digitalRead(SW_ENC) ) {
     but = SW_ENC_PULSADO;  
-    pulsado++;
-  }
-   
-  return but;
+    pressed++;
+  }   
+
 }
 
 int Hid::LeeEncoder( int state, int counter ) {
@@ -161,26 +170,56 @@ int Hid::LeeEncoder( int state, int counter ) {
       if (counter > 400 ) counter = 0;
     }
     PrintRPM( counter );
-    PrintVol( pump_.getVolume(counter) );
+    PrintVol( pump_.getVol ( counter ) );
   }
   return counter;  
+}
+
+int ReadPot( int pot , int antpot ) {
+  if (pot == (antpot+1) || pot == (antpot-1)){
+    return antpot;
+  } else {
+    return pot; 
+  }
 }
 
 int Hid::getEncoder( int state, int counter ) {
   return LeeEncoder( state , counter);
 }
 
+bool Hid::ButPressed() 
+{
+  if (pressed > trigger) { // Debouncing
+    return true;      
+  } else {
+    return false;
+  }
+}
+
 byte Hid::getButton()
 {
-  byte hid = Lee();
-  if (hid != 0 && pulsado > threshold)
-  {
-    but = 0;
-    pulsado = 0;
-    return Lee();
+  Read();
+  return but;
+}
+
+void Hid::ReadButton() {
+  Read();
+}
+
+void Hid::Reset() {
+  but = 0;
+  pressed = 0;  
+}
+
+int Hid::getRPM() {
+  return rpm;
+}
+
+void Hid::setRPM( long valor , int antvalor ) {
+  if (int(valor) > antvalor+2 || int(valor) < antvalor-2) {
+    rpm = int(valor * 400 / 1023);
+  } else {
+    rpm = antvalor;
   }
-  else
-  {
-    return 0;
-  }
+  
 }
